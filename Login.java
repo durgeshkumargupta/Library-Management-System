1cd8656ad3df5ecf991a75d31f10b257f1011588
durgeshkumargupta <47785309+durgeshkumargupta@users.noreply.github.com>
2019-04-30 20:57:20 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -40,10 +40,10 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jTextField3 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        Login = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        Signup = new javax.swing.JButton();
+        Forgot_Password = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jPasswordField1 = new javax.swing.JPasswordField();
 
@@ -58,30 +58,30 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setText("Login");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Login.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Login.setText("Login");
+        Login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                LoginActionPerformed(evt);
             }
         });
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setText("Username");
 
-        jButton2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton2.setText("Signup");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Signup.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Signup.setText("Signup");
+        Signup.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                SignupActionPerformed(evt);
             }
         });
 
-        jButton3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton3.setText("Forgot Password");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        Forgot_Password.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Forgot_Password.setText("Forgot Password");
+        Forgot_Password.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                Forgot_PasswordActionPerformed(evt);
             }
         });
 
@@ -101,15 +101,15 @@ public class Login extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(60, 60, 60)
-                .addComponent(jButton3)
+                .addComponent(Forgot_Password)
                 .addGap(58, 58, 58))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(43, 43, 43)
-                        .addComponent(jButton1)
+                        .addComponent(Login)
                         .addGap(52, 52, 52)
-                        .addComponent(jButton2))
+                        .addComponent(Signup))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -136,10 +136,10 @@ public class Login extends javax.swing.JFrame {
                     .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(67, 67, 67)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(Login)
+                    .addComponent(Signup))
                 .addGap(27, 27, 27)
-                .addComponent(jButton3)
+                .addComponent(Forgot_Password)
                 .addContainerGap())
         );
 
@@ -163,7 +163,7 @@ public class Login extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>                        
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    private void LoginActionPerformed(java.awt.event.ActionEvent evt) {                                      
         // TODO add your handling code here:
         String sql="select * from Account where Username=? and Password=?";
         try
@@ -199,22 +199,22 @@ public class Login extends javax.swing.JFrame {
                 
                 }
         }
-    }                                        
+    }                                     
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    private void SignupActionPerformed(java.awt.event.ActionEvent evt) {                                       
         // TODO add your handling code here:
         setVisible(false);
         Signup ob=new Signup();
         ob.setVisible(true);
-    }                                        
+    }                                      
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    private void Forgot_PasswordActionPerformed(java.awt.event.ActionEvent evt) {                                                
         // TODO add your handling code here:
         setVisible(false);
         Forgot ob=new Forgot();
         ob.setVisible(true);
         
-    }                                        
+    }                                               
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {                                            
         // TODO add your handling code here:
@@ -261,9 +261,9 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify                     
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton Forgot_Password;
+    private javax.swing.JButton Login;
+    private javax.swing.JButton Signup;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
