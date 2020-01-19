0907b54837f9b2f1c0208ae0e611c1964497d722
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2020-01-19 18:11:05 +0530

    Add files via upload

---
+++ java File/javaconnect.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Durgesh
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+   
+    public static Connection ConnecrDb()
+    {
+           try
+        {
+            System.out.println("Conneted ");
+          Class.forName("org.sqlite.JDBC");  
+           Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Durgesh\\Documents\\NetBeansProjects\\Libarary Management System\\LibraryNew.db");
+          return conn;
+        }catch(ClassNotFoundException | SQLException e)
+        {
+            
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+}
+        
