ccefb6f40eed2561ef6d21ff40004c90c8df5a83
Durgesh <47785309+durgeshkumargupta@users.noreply.github.com>
2020-01-19 17:50:35 +0530

    Update Home.java

--- Home.java
+++ Home.java
@@ -450,10 +450,8 @@ public class Home extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Home().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Home().setVisible(true);
         });
     }
 
